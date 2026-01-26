ebf1a5c5dd8244efe24f11b893a43d945834ebde
jens-buehnen <jensLMAO@proton.me>
2026-01-26 15:15:56 +0100

    update

--- clean-up.sh
+++ clean-up.sh
@@ -4,7 +4,7 @@ rm -rf $(pwd)/x86_64/*/pkg/
 rm -rf $(pwd)/x86_64/*/src/
 rm -rf $(pwd)/x86_64/*/*.tar.gz
 rm -rf $(pwd)/x86_64/*/*.tgz
-rm -rf $(pwd)/x86_64/dot-files/dot-files
+rm -rf $(pwd)/x86_64/dotfiles/dotfiles
 rm -rf $(pwd)/x86_64/freetube-nightly/*.xz
 rm -rf $(pwd)/x86_64/pandora_launcher/*.png
 rm -rf $(pwd)/x86_64/pandora_launcher/PandoraLauncher-Linux*
