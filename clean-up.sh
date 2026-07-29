8ea2e221e6d65a11e98e5c71c1d698571aa159e5
jens-buehnen <jensLMAO@proton.me>
2026-07-29 09:08:28 +0200

    update

--- clean-up.sh
+++ clean-up.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 rm -rf $(pwd)/x86_64/*/pkg
+rm -rf $(pwd)/x86_64/dotfiles/*/pkg
 rm -rf $(pwd)/x86_64/*/src
+rm -rf $(pwd)/x86_64/dotfiles/*/src
 rm -rf $(pwd)/x86_64/*/*.tar.gz
 rm -rf $(pwd)/x86_64/*/*.tgz
 rm -rf $(pwd)/x86_64/*/*.zip
@@ -9,5 +11,5 @@ rm -rf $(pwd)/x86_64/*/*.svg
 rm -rf $(pwd)/x86_64/*/*.xz
 rm -rf $(pwd)/x86_64/*/*.png
 rm -rf $(pwd)/x86_64/*/*.pkg.tar.zst
-rm -rf $(pwd)/x86_64/dotfiles/dotfiles
+rm -rf $(pwd)/x86_64/dotfiles/*/dotfiles
 rm -rf $(pwd)/x86_64/pandora_launcher/PandoraLauncher-Linux-*
